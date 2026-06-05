39f6245631279c9be72824f6beba154f42033606
JHYY <15901598712@163.com>
2026-06-05 14:55:15 +0800

    更新vscode配置

---
+++ test_run.s
@@ -0,0 +1,37 @@
+.text
+.balign 16
+.globl factorial
+factorial:
+	endbr64
+	movl $1, %eax
+	movl $1, %edx
+Lbb2:
+	cmpl %ecx, %edx
+	jg Lbb4
+	imull %edx, %eax
+	addl $1, %edx
+	jmp Lbb2
+Lbb4:
+	ret
+/* end function factorial */
+
+.text
+.balign 16
+.globl main_jhyy
+main_jhyy:
+	endbr64
+	pushq %rbp
+	movq %rsp, %rbp
+	subq $32, %rsp
+	movl $5, %ecx
+	callq factorial
+	movl %eax, %ecx
+	subq $-32, %rsp
+	subq $32, %rsp
+	callq puts
+	subq $-32, %rsp
+	movl $0, %eax
+	leave
+	ret
+/* end function main_jhyy */
+
